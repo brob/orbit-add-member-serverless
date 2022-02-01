fb9238351a60873dca3897dc4f40ef6b13f947bc
brob <bryanlrobinson@gmail.com>
2022-02-01 09:41:04 -0500

    Adds authentication requirement

--- netlify/functions/addMember/addMember.js
+++ netlify/functions/addMember/addMember.js
@@ -4,8 +4,17 @@ const fetch = require('node-fetch').default
 const handler = async (event) => {
   try {
     const { source, username, tags='' } = event.queryStringParameters
+
+    const apiKey = event.headers['x-api-key']
+
+    if (!apiKey) return {
+      statusCode: 401,
+      body: JSON.stringify({
+        error: 'Missing API key'
+      })
+    }
     console.log(event.queryStringParameters)
-    if (!(source && username)) return {
+    if (source === '' || username === '') return {
       statusCode: 500,
       body: JSON.stringify({
         error: 'Need both Source and username'
@@ -19,7 +28,7 @@ const handler = async (event) => {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.ORBIT_API_KEY}`
+        Authorization: `Bearer ${apiKey}`
       },
       body: JSON.stringify({
         identity: {
@@ -31,9 +40,12 @@ const handler = async (event) => {
         }
       })
     };
-
+    
     const response = await fetch(`https://app.orbit.love/api/v1/${process.env.ORBIT_WORKSPACE}/members`, options)
     const json = await response.json()
+
+    if (json.error) throw new Error(json.error)
+
     const member = json.data
     const { id, attributes } = member
     const { name } = attributes
